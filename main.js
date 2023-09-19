96b35349761e59f96b11c7828cda04ab04d8b699
bao-th <3283706085@qq.com>
2023-09-19 14:04:57 +0800

    feat

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    // win.loadFile('dist/index.html')
+    const url = "http://localhost:8019";
+    win.loadURL(url);
+    // 调试
+    // win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
